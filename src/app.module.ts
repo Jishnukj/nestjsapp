b328ef7784eec3e5b3bfd75ce50adc57c34eee21
Jishnukj <jishnukjayan97@gmail.com>
2021-06-17 16:56:52 +0530

    entity created

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { config } from './orm.config';
+import { CompaniesModule } from './companies/companies.module';
 @Module({
-  imports: [TypeOrmModule.forRoot(config)],
+  imports: [TypeOrmModule.forRoot(config), CompaniesModule],
   controllers: [AppController],
   providers: [AppService],
 })
