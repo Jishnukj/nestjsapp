152dfce78508df1507b606c6e37a263644a595d8
Jishnukj <jishnukjayan97@gmail.com>
2021-06-18 10:19:38 +0530

    remove unwanted and swagger added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-//import { config } from './orm.config';
-import { CompaniesModule } from './companies/companies.module';
 import { DatabaseModule } from './database/database.module';
 import { TestresourceModule } from './testresource/testresource.module';
 @Module({
-  imports: [ DatabaseModule, CompaniesModule, TestresourceModule],
+  imports: [ DatabaseModule, TestresourceModule],
   controllers: [AppController],
   providers: [AppService],
 })
