135929136912261595daed23ec09f03846047924
Jishnukj <jishnukjayan97@gmail.com>
2021-06-18 10:26:54 +0530

    swagger

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Nestjs example')
+    .setDescription('A Nestjs API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('nest')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
