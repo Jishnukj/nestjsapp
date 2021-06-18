e4456f218c6e169d2d0887850eb15b4b68147abd
Jishnukj <jishnukjayan97@gmail.com>
2021-06-18 08:04:09 +0530

    n

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -1,4 +1,11 @@
 import { Controller } from '@nestjs/common';
-
+import { Crud, CrudController } from '@nestjsx/crud';
+import { Company } from './company.entity';
+import { CompaniesService } from './companies.service';
+@Crud({model:{
+    type: Company
+}})
 @Controller('companies')
-export class CompaniesController {}
+export class CompaniesController{
+    constructor(public service: CompaniesService){}
+}
