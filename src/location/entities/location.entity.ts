b77f6b1f62e95b176d2aba031848dee500252b6f
jishnukj <jishnukjayan97@gmail.com>
2021-06-23 11:42:38 +0530

    formatting

--- src/location/entities/location.entity.ts
+++ src/location/entities/location.entity.ts
@@ -4,18 +4,17 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 
 @Entity('locations')
-export class Locations{
+export class Locations {
     @PrimaryGeneratedColumn()
-    id?:number;
+    id?: number;
 
-    @Column({type: 'varchar', length:100, nullable:false})
-    location:string;
+    @Column({ type: 'varchar', length: 100, nullable: false })
+    location: string;
 
-    @Column({type: 'int', nullable:false})
-    pincode:number;
-    
+    @Column({ type: 'int', nullable: false })
+    pincode: number;
+
     // this will not create column
-    @OneToMany(() => Subloc, subloc=>subloc.locations)
+    @OneToMany(() => Subloc, subloc => subloc.locations)
     sublocs: Subloc[];
-    
 }
