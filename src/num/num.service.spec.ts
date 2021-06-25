dadd5a17c4e2240acad75443b4bf13b5472c68ca
jishnukj <jishnukjayan97@gmail.com>
2021-06-25 14:13:52 +0530

    service unit test

--- src/num/num.service.spec.ts
+++ src/num/num.service.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Num } from './entities/num.entity';
 import { NumService } from './num.service';
 
 describe('NumService', () => {
   let service: NumService;
 
+
+
+
+
+
+  const mockNumRepo = {
+    create: jest.fn().mockImplementation(dto => dto),
+    save: jest.fn().mockImplementation(dto => Promise.resolve({ id: Date.now(), ...dto })),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [NumService],
+      providers: [NumService,
+        {
+          provide: getRepositoryToken(Num),
+          useValue: mockNumRepo
+        }
+      ],
     }).compile();
 
     service = module.get<NumService>(NumService);
@@ -15,4 +32,9 @@ describe('NumService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('it should create a new num', async () => {
+    const data = { name: 'jishnu' }
+    expect(await service.create(data)).toEqual({ id: expect.any(Number), ...data })
+  })
 });
