2351993940a80457b72f8b0e62e6c7a6063fb45f
jishnukj <jishnukjayan97@gmail.com>
2021-06-25 10:57:54 +0530

    controller unit test

--- src/subloc/subloc.controller.spec.ts
+++ src/subloc/subloc.controller.spec.ts
@@ -1,15 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Data } from 'src/datas/entities/data.entity';
 import { SublocController } from './subloc.controller';
 import { SublocService } from './subloc.service';
 
 describe('SublocController', () => {
   let controller: SublocController;
 
+  const mockSublocService = {
+    create: jest.fn(dto => {
+      return { id: 2, ...dto }
+    })
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SublocController],
       providers: [SublocService],
-    }).compile();
+    }).overrideProvider(SublocService).useValue(mockSublocService).compile();
 
     controller = module.get<SublocController>(SublocController);
   });
@@ -17,4 +24,20 @@ describe('SublocController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+  
+  it('create a subloc', () => {
+    const dto = { name: 'jishnu', locationsId: 1 };
+    expect(controller.create(dto)).toEqual(
+      {
+        id: expect.any(Number),
+        name: 'jishnu',
+        locationsId: 1
+      }
+    );
+
+    expect(mockSublocService.create).toHaveBeenCalledWith(dto); 
+  });
+
+
+
 });
